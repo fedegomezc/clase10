5c9e5fd770966a42092d86041ed81ba7454e22d5
Federico Gomez Conde <fedeic8@gmail.com>
2023-07-08 19:26:53 -0300

    se utiliza splice y reduce para la resolución

---
+++ suma_sin_medios.js
@@ -0,0 +1,26 @@
+// suma sin centro
+// dado un array de números sumar todos los elementos a excepción del valor del centro.
+// ejemplos
+// [1, 2, 3, 4, 5] = 12    // se elimina el 3
+// [1, 2, 3, 4, 5, 6] = 14   // se eliminan 3 y 4
+
+// casos posibles -> [] , [1] , [1, 2] , [1,2,3,...,]
+
+let arreglo = [1, 2, 3, 4, 5, 6];
+
+let mitadArreglo = arreglo.length / 2
+let middleIndex = Math.round(mitadArreglo) - 1
+
+if(arreglo.length >= 3) {
+    arreglo.length % 2 === 0 ?
+    arreglo.splice(middleIndex, 2) :
+    arreglo.splice(middleIndex, 1)
+
+    console.log(arreglo);
+} 
+
+let suma = arreglo.reduce((acc, el) => {
+    return acc + el;
+}, 0);
+
+console.log(suma);
