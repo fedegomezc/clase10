f6b15d112f3147f09374e4a4d6e6b06765b4c93e
Federico Gomez Conde <fedeic8@gmail.com>
2023-07-09 16:04:46 -0300

    se agrega otra forma de resolverlo con reduce

--- fibonacci.js
+++ fibonacci.js
@@ -8,13 +8,28 @@ hasta = 3 resultado [0, 1, 1] /* suma 0 + 1 para obtener la última posición */
 /* hasta = 6 resultado [0, 1, 1, 2, 3, 5]
 hasta = 7 resultado [0, 1, 1, 2, 3, 5, 8] * */
 
-const fibonacci= (hasta) => {
-    const fibo = [0, 1];
+let hasta = process.argv[2];
 
-    for (let i = 2; i < hasta; i++) {
-        fibo[i] = fibo[i-1] + fibo[i-2]
+const fibonacci= (hasta) => {
+    let fibo = [0, 1];
+    if(hasta <= 1) {
+        fibo = [0];
+    } else {
+        for (let i = 2; i < hasta; i++) {
+            fibo[i] = fibo[i-1] + fibo[i-2]
+        }
     }
+    
+    
     return fibo;
 }
+console.log(fibonacci(hasta));
 
-console.log(fibonacci(7));
+
+/* function fib(n){
+    return new Array(n).fill(1).reduce((arr, _ ,i) => {
+        arr.push((i <= 1) ? i : arr[i-2] + arr[i-1])
+        return arr
+    },[]) ;
+  }
+  console.log(fib(hasta*1)) */
