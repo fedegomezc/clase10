f705337982c973d775e140e93b28e4281ebd2c76
Federico Gomez Conde <fedeic8@gmail.com>
2023-07-13 17:26:58 -0300

    anotaciones cambio de enfoque del problema

--- cubomagico/cubomagico.js
+++ cubomagico/cubomagico.js
@@ -1,10 +1,10 @@
 // cantidad de caras = 6 con 9 unidades cada uno
-// frontal - ROJO
-// derecha - AZUL
-// izquierda - VERDE
-// trasera - NARANJA
-// abajo - AMARILLO
-// arriba - BLANCO
+// frontal - ROJO         eje +x
+// derecha - AZUL         eje +y
+// izquierda - VERDE      eje -y
+// trasera - NARANJA      eje -x
+// abajo - AMARILLO       eje -z
+// arriba - BLANCO        eje +z
 
 // clases de piezas: 
 //         centros -> 6 ( 1 color ) 
@@ -12,29 +12,24 @@
 //         aristas  -> 12 ( 2 colores )
 // total 26 piezas
 
-const colores = {
-    frontal: 'ROJO',
-    derecha: 'AZUL',
-    izquierda: 'VERDE',
-    trasera: 'NARANJA',
-    abajo: 'AMARILLO',
-    arriba: 'BLANCO'
-};
+// cada pieza debe tener un vector (x', y', z') que es relativo a un vector global (x) centrado en el cubo
 
-const cubo = [];
+// al rotar a 90° los ejes de la pieza cambian de direccion
+/* var transitions = {
+    'x': {'y': 'z', 'z': 'y'},
+    'y': {'x': 'z', 'z': 'x'},
+    'z': {'x': 'y', 'y': 'x'}
+  } 
+*/
 
-for (let i = 0; i < 6; i++) {
-    cubo[i] = [];
-    for (let j = 0; j < 3; j++) {
-        cubo[i][j] = [];
-        for (let k = 0; k < 3; k++) {
-            // Obtener el nombre de la cara correspondiente al índice i
-            const cara = Object.keys(colores)[i];
-            
-            // Asignar el color correspondiente a la cara
-            cubo[i][j][k] = colores[cara];
-        }
-    }
-}
-
-console.log(cubo);
+// cada pieza tiene una posicion x, y, z y una orientación xa, ya, za.
+// esquinas 8 piezas
+const piezaFLU = [x, y, z, xa, ya, za]    // Front Left Up
+const piezaFLD = [x, y, z, xa, ya, za]    // Front Left Down
+const piezaFRU = [x, y, z, xa, ya, za]    // Front Right Up
+const piezaFRD = [x, y, z, xa, ya, za]    // Front Right Down
+const piezaBLU = [x, y, z, xa, ya, za]    // Back Left Up
+const piezaBLD = [x, y, z, xa, ya, za]    // Back Left Down
+const piezaBRU = [x, y, z, xa, ya, za]    // Back Right Up
+const piezaBRD = [x, y, z, xa, ya, za]    // Back Right Down
+// aristas
